4aaa1a6dd77a6e1ae3bff63583eb11117e200b4d
jinhao yu <2100899780@qq.com>
2023-12-26 19:10:52 +0800

    搭建项目骨架

--- backend/src/main/java/com/kob/backend/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/pk/IndexController.java
@@ -4,12 +4,18 @@ package com.kob.backend.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk")
 public class IndexController {
     @RequestMapping("/index")
-    public String index() {
-        return "hhh";
+    public Map<String, String> index() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","1500");
+        return bot1;
     }
 
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>昵称：{{ name }}</div>
+  <div>得分：{{ rating }}</div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+export default{
+  name: "App",
+  setup: () => {
+    let name = ref("");
+    let rating = ref("");
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/pk/index",
+      type:"get",
+      success: resp => {
+        console.log(resp);
+        name.value = resp.name;
+        rating.value = resp.rating;
+      }
+    })
+    return {
+      name,
+      rating
+    }
+  }
+}
+
+
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  body{
+    background-image: url("@/assets/background.jfif");
+    background-size: cover;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
